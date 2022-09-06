b8be50a6abb67cc93f51194a3734252fa0a362a8
cp <1259945111@qq.com>
2022-09-06 18:12:59 +0800

    feat:登录页完成布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/login',
+    component: () => import('@/views/login') // 路由懒加载，使用倒了路由界面再去请求
+  }
 ]
 
 const router = new VueRouter({
